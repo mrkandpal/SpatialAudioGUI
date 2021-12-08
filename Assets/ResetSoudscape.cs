043a27422d592c10363c912703c5447caf5b70d9
mrkandpal <devansh.kandpal@gmail.com>
2021-12-08 16:05:18 +0100

    Added graphics and resized elements

--- Assets/ResetSoudscape.cs
+++ Assets/ResetSoudscape.cs
@@ -1,6 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System;
 using UnityEngine.UI;
 
 public class ResetSoudscape : MonoBehaviour
@@ -10,10 +16,22 @@ public class ResetSoudscape : MonoBehaviour
     public GameObject sourceParent;
     public GameObject audioDropZone;
 
+    //Define UDP port number and IP address to send data to MATLAB
+    public int port;
+    public string IP = "127.0.0.1";
+
+    //UDP Conncetion elements
+    IPEndPoint remoteEndPoint;
+    UdpClient client;
+
+    //Initialise string to contain reset status message
+    public string statusMessage = "";
+
     // Start is called before the first frame update
     void Start()
     {
         resetButton.onClick.AddListener(resetAllSources);
+        init();
     }
     
     void resetAllSources()
@@ -31,11 +49,40 @@ public class ResetSoudscape : MonoBehaviour
         {
             dropped.sourceActivationFlags[i] = 0;
         }
+
+        sendResetData();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Initialise UDP
+    public void init()
+    {
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(this.IP), this.port);
+        client = new UdpClient();
+    }
+
+    void sendResetData()
+    {
+        this.statusMessage = "Scene Reset";
+        SendString(this.statusMessage);
+    }
+
+    void SendString(string message)
+    {
+        try
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            client.Send(data, data.Length, remoteEndPoint);
+        }
+        catch (Exception err)
+        {
+            Debug.Log(err.ToString());
+            //Debug.Log("error here");
+        }
     }
 }
